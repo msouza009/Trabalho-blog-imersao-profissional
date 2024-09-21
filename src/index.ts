b79e7ac585dd373c0ac3eeb71baf39cac32a6834
Matheus <ra-23371012-2@alunos.unicesumar.edu.br>
2024-09-21 16:00:10 -0300

    inicio projeto

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response} from "express";
+import { connect } from "http2";
 import mysql from "mysql2/promise";
 
 const app = express();
 
-// Configura EJS como a engine de renderização de templates
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
@@ -15,36 +15,22 @@ const connection = mysql.createPool({
     database: "unicesumar"
 });
 
-// Middleware para permitir dados no formato JSON
 app.use(express.json());
-// Middleware para permitir dados no formato URLENCODED
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}))
 
-app.get('/categories', async function (req: Request, res: Response) {
-    const [rows] = await connection.query("SELECT * FROM categories");
-    return res.render('categories/index', {
-        categories: rows
-    });
+app.get('/users', async function (req: Request, res: Response) {
+    try {
+        const [rows] = await connection.query("SELECT * FROM users");
+        return res.render('users/index', {
+            users: rows
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Erro ao buscar usuários");
+    }
 });
 
-app.get("/categories/form", async function (req: Request, res: Response) {
-    return res.render("categories/form");
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-app.post("/categories/save", async function(req: Request, res: Response) {
-    const body = req.body;
-    const insertQuery = "INSERT INTO categories (name) VALUES (?)";
-    await connection.query(insertQuery, [body.name]);
-
-    res.redirect("/categories");
-});
-
-app.post("/categories/delete/:id", async function (req: Request, res: Response) {
-    const id = req.params.id;
-    const sqlDelete = "DELETE FROM categories WHERE id = ?";
-    await connection.query(sqlDelete, [id]);
-
-    res.redirect("/categories");
-});
-
-app.listen('3000', () => console.log("Server is listening on port 3000"));
